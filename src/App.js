b766278a2bbb0128b3ba049474b401fa9856233b
Anchal899 <101783656+Anchal899@users.noreply.github.com>
2024-07-27 19:33:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import RuleForm from './components/RuleForm';
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Rule Master</h1>
-            <RuleForm />
+        <div className="App flex flex-col">
+        <div className='bg-blue-400 h-45 w-45'> 
+        <h1 className='text-4xl text-center my-2'>Rule Master</h1>
+        </div>
+           <div >
+           <RuleForm className='border border-cyan-400'/>
+           </div>
+            
         </div>
     );
 };
